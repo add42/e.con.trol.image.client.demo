901f7a2c05aeef2dd5a8210f8cf01f8eb5bd56a6
Arno Dewald <a.dewald@eckelmann.de>
2019-10-21 17:03:43 +0200

    BVS-1922: Extensions.GetPropertyValueFromJObject() hinzugefügt

---
+++ E.CON.TROL.CHECK.DEMO/Extensions.cs
@@ -0,0 +1,20 @@
+﻿using Newtonsoft.Json.Linq;
+
+namespace E.CON.TROL.CHECK.DEMO
+{
+    static class Extensions
+    {
+        public static string GetPropertyValueFromJObject(this JObject json, string propertyName)
+        {
+            JToken token = null;
+            if (json?.TryGetValue(propertyName, out token) == true)
+            {
+                return token?.ToString();
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+}
